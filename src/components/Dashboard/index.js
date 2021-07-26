a15495e94d0a62921aec57c1516e64195196c00a
Nicole Pimenta <niipimenta@hotmail.com>
2021-07-26 14:24:25 -0300

    ajuste

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -52,7 +52,7 @@ const Dashboard = ({ authenticated, user }) => {
           },
         }
       )
-      .then((_) => loadTechs())
+      .then((response) => setTech([response.data]))
       .catch((err) => console.err);
   };
 
